9c37a9c1c280e4002f5cae4d514368a66f969c6a
Albert NIYONSENGA <94793290+Al04ni@users.noreply.github.com>
2025-01-07 17:47:41 +0200

    modified Saving path

--- Sample QRCode/sample1.py
+++ Sample QRCode/sample1.py
@@ -1,10 +1,11 @@
 import qrcode
 import os
+
 #creating the qrcode object with larger size and higher error handling
 qr = qrcode.QRCode(version=3, box_size=20, border=10, error_correction=qrcode.constants.ERROR_CORRECT_H)
 
 # Define the data to be encoded in the QR code
-data = "https://github.com/Al04ni/qrcode_generator" #feel free to add text or a link
+data = "https://github.com/Al04ni/qrcode_generator" #Feel free to add text or a link
 
 # Add the data to the QR code object
 qr.add_data(data)
@@ -19,9 +20,8 @@ img = qr.make_image(fill_color="black", back_color="white")
 output_folder =os.path.join(os.getcwd(), "Samples") 
 os.makedirs(output_folder, exist_ok=True)
 
-#Defining the file name
+#Defining the file name &full path to save the file
 file_name = "qrcode_sample1.png"
-# Define the full path to save the file
 file_path = os.path.join(output_folder, file_name)
 
 # Save the QR code image
